3bcd71941ce7f1c8eafad0daf8517acf97de49d0
anutaluzhetska <anyluzeckaana.study@gmail.com>
2026-03-18 21:33:12 +0200

    add functions to add, update and delete cats

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import createError from 'http-errors'
-import express from 'express'
-import path from 'path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
-import weaponRouter from './routes/dead_space.js'
-import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+dotenv.config();
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import weaponRouter from "./routes/dead_space.js";
+import catsRouter from "./routes/cats.js";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,33 +17,34 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/weapons', weaponRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/weapons", weaponRouter);
+app.use("/cats", catsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;
